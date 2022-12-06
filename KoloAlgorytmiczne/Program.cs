e5c353ae50bcd9c5843121eb30d207054e0a7074
Kuba Bielecki <kuba.bielecki@outlook.com>
2022-12-06 09:08:09 +0100

    Add project files.

---
+++ KoloAlgorytmiczne/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace KoloAlgorytmiczne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyPow(2.0, -2147483648));
+        }
+
+        static double MyPow(double x, long n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (n < 0)
+            {
+                n *= -1;
+                x = 1 / x;
+            }
+
+            if (n % 2 == 0)
+            {
+                return MyPow(x * x, n / 2);
+            }
+            else
+            {
+                return x * MyPow(x * x, n / 2);
+            }
+        }
+    }
+}
